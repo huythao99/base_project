39ee294588ce968852cf670b5a5cc3a3ea2fe456
nhthao <ngohuythao99@gmail.com>
2023-03-08 23:14:09 +0700

    update feature shader_rn_skia

--- src/features/home/home-screen.tsx
+++ src/features/home/home-screen.tsx
@@ -1,46 +1,118 @@
+import React from 'react';
 import {
   Canvas,
-  Circle,
-  Oval,
-  Group,
-  SweepGradient,
+  Skia,
+  Shader,
+  Fill,
   vec,
+  useTouchHandler,
+  useValue,
+  useComputedValue,
+  useClockValue,
 } from '@shopify/react-native-skia';
-import * as React from 'react';
-import {BLUE_50, BLUE_A200, BLUE_A400} from '../../constants/colors';
-import useHome from './home-hooks';
-import styles from './styles';
+import {useWindowDimensions} from 'react-native';
+import {
+  BLUE_A200,
+  RED_A400,
+  YELLOW_700,
+  BLUE_GRAY,
+} from '../../constants/colors';
+
+const source = Skia.RuntimeEffect.Make(`
+uniform float4 colors[4];
+uniform float2 center;
+uniform float2 pointer;
+uniform float clock;
+const float4 black = vec4(0, 0, 0, 1);
+struct Paint {
+  float4 color;
+  bool stroke;
+  float strokeWidth;
+};
+
+float sdCross( in vec2 p, in vec2 b, float r ) 
+{
+    p = abs(p); p = (p.y>p.x) ? p.yx : p.xy;
+    
+	vec2  q = p - b;
+    float k = max(q.y,q.x);
+    vec2  w = (k>0.0) ? q : vec2(b.y-p.x,-k);
+    float d = length(max(w,0.0));
+    return ((k>0.0)?d:-d) + r;
+}
+
+
+
+float sdCircle(vec2 p, float r) {
+  return length(p) - r;
+}
+
+float sdLine(vec2 p, vec2 a, vec2 b) {
+  vec2 pa = p - a;
+  vec2 ba = b - a;
+  float h = saturate(dot(pa, ba) / dot(ba, ba));
+  return length(pa - ba * h);
+}
+float4 draw(float4 color, float d, Paint paint) {
+  bool isFill = !paint.stroke && d < 0;
+  bool isStroke = paint.stroke && abs(d) < paint.strokeWidth/2;
+  if (isFill || isStroke) {
+    return paint.color;
+  }
+  return color;
+}
+float4 drawCircle(float4 color, float2 p, float radius, Paint paint) {
+  float d = sdCircle(p, radius);
+  return draw(color, d, paint);
+}
+float4 drawLine(float4 color, float2 p, float2 a, float2 b, Paint paint) {
+  float d = sdLine(p, a, b);
+  return draw(color, d, paint);
+}
+vec4 main(vec2 xy) {
+  float4 color = black;
+  float strokeWidth = 30;
+  float radius = center.x - strokeWidth/2;
+  Paint pointPaint = Paint(black, false, 0);
+  Paint paint = Paint(colors[3], false, 0);
+  float d = sdCross(xy - center, pointer, radius) ;
+  float offset = -clock * 0.18;
+  float i = mod(floor((d + offset)/strokeWidth), 4);
+  if (i == 0) {
+    color = colors[0];
+  } else if (i == 1) {
+    color = colors[1];
+  } else if (i == 2) {
+    color = colors[2];
+  } else if (i == 3) {
+    color = colors[3];
+  }
+  return color; 
+}`)!;
+
+const colors = [RED_A400, BLUE_A200, BLUE_GRAY, YELLOW_700].map(c =>
+  Skia.Color(c),
+);
 
 export default function HomeScreen() {
-  const {
-    center,
-    rotationTransform,
-    rotationTransformSecond,
-    rotationTransformLogo,
-  } = useHome();
-
+  const {width, height} = useWindowDimensions();
+  const clock = useClockValue();
+  const center = vec(width / 2, height / 2);
+  const pointer = useValue(vec(0, 0));
+  const onTouch = useTouchHandler({
+    onActive: e => {
+      pointer.current = e;
+    },
+  });
+  const uniforms = useComputedValue(
+    () => ({colors, center, pointer: pointer.current, clock: clock.current}),
+    [pointer, clock],
+  );
   return (
-    <Canvas style={styles.container}>
-      <Circle cx={center.x} cy={center.y} r={15} color="lightblue">
-        <SweepGradient c={vec(center.x, center.y)} colors={[BLUE_A200]} />
-      </Circle>
-      <Group
-        style={'stroke'}
-        strokeWidth={10}
-        transform={rotationTransformLogo}
-        origin={center}>
-        <SweepGradient
-          c={vec(center.x, center.y)}
-          colors={[BLUE_50, BLUE_A200, BLUE_A400]}
-        />
-        <Oval x={center.x - 150} y={center.y - 50} width={300} height={100} />
-        <Group transform={rotationTransform} origin={center}>
-          <Oval x={center.x - 150} y={center.y - 50} width={300} height={100} />
-        </Group>
-        <Group transform={rotationTransformSecond} origin={center}>
-          <Oval x={center.x - 150} y={center.y - 50} width={300} height={100} />
-        </Group>
-      </Group>
+    <Canvas style={{flex: 1}} onTouch={onTouch}>
+      <Fill>
+        <Shader source={source} uniforms={uniforms} />
+      </Fill>
     </Canvas>
   );
 }
